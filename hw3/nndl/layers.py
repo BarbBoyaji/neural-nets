3d265234defa462800a9769451a53e7f412e7f4c
Barbara <boyaji99@gmail.com>
2020-02-04 21:39:12 -0800

    modular fc implementation

--- hw3/nndl/layers.py
+++ hw3/nndl/layers.py
@@ -13,176 +13,209 @@ cs231n.stanford.edu.
 
 
 def affine_forward(x, w, b):
-  """
-  Computes the forward pass for an affine (fully-connected) layer.
+    """
+    Computes the forward pass for an affine (fully-connected) layer.
 
-  The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
-  examples, where each example x[i] has shape (d_1, ..., d_k). We will
-  reshape each input into a vector of dimension D = d_1 * ... * d_k, and
-  then transform it to an output vector of dimension M.
+    The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+    examples, where each example x[i] has shape (d_1, ..., d_k). We will
+    reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+    then transform it to an output vector of dimension M.
 
-  Inputs:
-  - x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
-  - w: A numpy array of weights, of shape (D, M)
-  - b: A numpy array of biases, of shape (M,)
-  
-  Returns a tuple of:
-  - out: output, of shape (N, M)
-  - cache: (x, w, b)
-  """
+    Inputs:
+    - x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
+    - w: A numpy array of weights, of shape (D, M)
+    - b: A numpy array of biases, of shape (M,)
 
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Calculate the output of the forward pass.  Notice the dimensions
-  #   of w are D x M, which is the transpose of what we did in earlier 
-  #   assignments.
-  # ================================================================ #
+    Returns a tuple of:
+    - out: output, of shape (N, M)
+    - cache: (x, w, b)
+    """
 
-  pass
-
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Calculate the output of the forward pass.  Notice the dimensions
+    #   of w are D x M, which is the transpose of what we did in earlier 
+    #   assignments.
+    # ================================================================ #
     
-  cache = (x, w, b)
-  return out, cache
+    #find the dimensons, D
+    D, M = w.shape
+    
+    #get x_shape
+    x_shape = x.shape
+    #print(x_shape)
+    
+    #construct reshaped x
+    x_new = x.reshape((x_shape[0], D))
+
+    #verify final shape
+    #print(x_new.shape)
+        
+    #construct out
+    out = np.dot(x_new, w) + b[np.newaxis,:]
+    pass
+
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
+
+    cache = (x, w, b)
+    return out, cache
 
 
 def affine_backward(dout, cache):
-  """
-  Computes the backward pass for an affine layer.
+    """
+    Computes the backward pass for an affine layer.
 
-  Inputs:
-  - dout: Upstream derivative, of shape (N, M)
-  - cache: Tuple of:
+    Inputs:
+    - dout: Upstream derivative, of shape (N, M)
+    - cache: Tuple of:
     - x: Input data, of shape (N, d_1, ... d_k)
     - w: Weights, of shape (D, M)
 
-  Returns a tuple of:
-  - dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
-  - dw: Gradient with respect to w, of shape (D, M)
-  - db: Gradient with respect to b, of shape (M,)
-  """
-  x, w, b = cache
-  dx, dw, db = None, None, None
+    Returns a tuple of:
+    - dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
+    - dw: Gradient with respect to w, of shape (D, M)
+    - db: Gradient with respect to b, of shape (M,)
+    """
+    x, w, b = cache
+    dx, dw, db = None, None, None
 
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Calculate the gradients for the backward pass.
-  # ================================================================ #
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Calculate the gradients for the backward pass.
+    # ================================================================ #
+    
+    # dout is N x M
+    # dx should be N x d1 x ... x dk; it relates to dout through multiplication with w, which is D x M
+    # dw should be D x M; it relates to dout through multiplication with x, which is N x D after reshaping
+    # db should be M; it is just the sum over dout examples
+    
+    #find the dimensons, D
+    D, M = w.shape
+    #print(f"D: {D}, M: {M}")
+    
+    #get x_shape
+    x_shape = x.shape
+    #print(f"N: {x_shape[0]}")
+    
+    #construct reshaped x
+    x_new = x.reshape((x_shape[0],D))
+    
+    dw = np.dot(x_new.T,dout)
+    
+    db = dout.sum(axis=0)
+    
+    dx = np.dot(dout, w.T)
+    dx = dx.reshape(x_shape)
+        
+    pass
 
-  # dout is N x M
-  # dx should be N x d1 x ... x dk; it relates to dout through multiplication with w, which is D x M
-  # dw should be D x M; it relates to dout through multiplication with x, which is N x D after reshaping
-  # db should be M; it is just the sum over dout examples
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
 
-  pass
-
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
-  
-  return dx, dw, db
+    return dx, dw, db
 
 def relu_forward(x):
-  """
-  Computes the forward pass for a layer of rectified linear units (ReLUs).
+    """
+    Computes the forward pass for a layer of rectified linear units (ReLUs).
 
-  Input:
-  - x: Inputs, of any shape
+    Input:
+    - x: Inputs, of any shape
 
-  Returns a tuple of:
-  - out: Output, of the same shape as x
-  - cache: x
-  """
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement the ReLU forward pass.
-  # ================================================================ #
+    Returns a tuple of:
+    - out: Output, of the same shape as x
+    - cache: x
+    """
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement the ReLU forward pass.
+    # ================================================================ #
+    out = x*(x>0)
+    pass
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
 
-  pass
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
- 
-  cache = x
-  return out, cache
+    cache = x
+    return out, cache
 
 
 def relu_backward(dout, cache):
-  """
-  Computes the backward pass for a layer of rectified linear units (ReLUs).
+    """
+    Computes the backward pass for a layer of rectified linear units (ReLUs).
 
-  Input:
-  - dout: Upstream derivatives, of any shape
-  - cache: Input x, of same shape as dout
+    Input:
+    - dout: Upstream derivatives, of any shape
+    - cache: Input x, of same shape as dout
 
-  Returns:
-  - dx: Gradient with respect to x
-  """
-  x = cache
+    Returns:
+    - dx: Gradient with respect to x
+    """
+    x = cache
 
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement the ReLU backward pass
-  # ================================================================ #
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement the ReLU backward pass
+    # ================================================================ #
+    dx = (x > 0) * dout
+    # ReLU directs linearly to those > 0
+    pass
 
-  # ReLU directs linearly to those > 0
-  pass
-    
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
- 
-  return dx
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
+
+    return dx
 
 def svm_loss(x, y):
-  """
-  Computes the loss and gradient using for multiclass SVM classification.
+    """
+    Computes the loss and gradient using for multiclass SVM classification.
 
-  Inputs:
-  - x: Input data, of shape (N, C) where x[i, j] is the score for the jth class
+    Inputs:
+    - x: Input data, of shape (N, C) where x[i, j] is the score for the jth class
     for the ith input.
-  - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+    - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
     0 <= y[i] < C
 
-  Returns a tuple of:
-  - loss: Scalar giving the loss
-  - dx: Gradient of the loss with respect to x
-  """
-  N = x.shape[0]
-  correct_class_scores = x[np.arange(N), y]
-  margins = np.maximum(0, x - correct_class_scores[:, np.newaxis] + 1.0)
-  margins[np.arange(N), y] = 0
-  loss = np.sum(margins) / N
-  num_pos = np.sum(margins > 0, axis=1)
-  dx = np.zeros_like(x)
-  dx[margins > 0] = 1
-  dx[np.arange(N), y] -= num_pos
-  dx /= N
-  return loss, dx
+    Returns a tuple of:
+    - loss: Scalar giving the loss
+    - dx: Gradient of the loss with respect to x
+    """
+    N = x.shape[0]
+    correct_class_scores = x[np.arange(N), y]
+    margins = np.maximum(0, x - correct_class_scores[:, np.newaxis] + 1.0)
+    margins[np.arange(N), y] = 0
+    loss = np.sum(margins) / N
+    num_pos = np.sum(margins > 0, axis=1)
+    dx = np.zeros_like(x)
+    dx[margins > 0] = 1
+    dx[np.arange(N), y] -= num_pos
+    dx /= N
+    return loss, dx
 
 
 def softmax_loss(x, y):
-  """
-  Computes the loss and gradient for softmax classification.
+    """
+    Computes the loss and gradient for softmax classification.
 
-  Inputs:
-  - x: Input data, of shape (N, C) where x[i, j] is the score for the jth class
+    Inputs:
+    - x: Input data, of shape (N, C) where x[i, j] is the score for the jth class
     for the ith input.
-  - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+    - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
     0 <= y[i] < C
 
-  Returns a tuple of:
-  - loss: Scalar giving the loss
-  - dx: Gradient of the loss with respect to x
-  """
+    Returns a tuple of:
+    - loss: Scalar giving the loss
+    - dx: Gradient of the loss with respect to x
+    """
 
-  probs = np.exp(x - np.max(x, axis=1, keepdims=True))
-  probs /= np.sum(probs, axis=1, keepdims=True)
-  N = x.shape[0]
-  loss = -np.sum(np.log(probs[np.arange(N), y])) / N
-  dx = probs.copy()
-  dx[np.arange(N), y] -= 1
-  dx /= N
-  return loss, dx
+    probs = np.exp(x - np.max(x, axis=1, keepdims=True))
+    probs /= np.sum(probs, axis=1, keepdims=True)
+    N = x.shape[0]
+    loss = -np.sum(np.log(probs[np.arange(N), y])) / N
+    dx = probs.copy()
+    dx[np.arange(N), y] -= 1
+    dx /= N
+    return loss, dx
