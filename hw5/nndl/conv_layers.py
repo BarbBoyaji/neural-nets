85183994099559a5583cf25e41135542d483b318
Barbara <boyaji99@gmail.com>
2020-02-25 18:36:25 -0800

    cnn layers

--- hw5/nndl/conv_layers.py
+++ hw5/nndl/conv_layers.py
@@ -13,139 +13,219 @@ cs231n.stanford.edu.
 """
 
 def conv_forward_naive(x, w, b, conv_param):
-  """
-  A naive implementation of the forward pass for a convolutional layer.
+    """
+    A naive implementation of the forward pass for a convolutional layer.
 
-  The input consists of N data points, each with C channels, height H and width
-  W. We convolve each input with F different filters, where each filter spans
-  all C channels and has height HH and width HH.
+    The input consists of N data points, each with C channels, height H and width
+    W. We convolve each input with F different filters, where each filter spans
+    all C channels and has height HH and width HH.
 
-  Input:
-  - x: Input data of shape (N, C, H, W)
-  - w: Filter weights of shape (F, C, HH, WW)
-  - b: Biases, of shape (F,)
-  - conv_param: A dictionary with the following keys:
+    Input:
+    - x: Input data of shape (N, C, H, W)
+    - w: Filter weights of shape (F, C, HH, WW)
+    - b: Biases, of shape (F,)
+    - conv_param: A dictionary with the following keys:
     - 'stride': The number of pixels between adjacent receptive fields in the
       horizontal and vertical directions.
     - 'pad': The number of pixels that will be used to zero-pad the input.
 
-  Returns a tuple of:
-  - out: Output data, of shape (N, F, H', W') where H' and W' are given by
+    Returns a tuple of:
+    - out: Output data, of shape (N, F, H', W') where H' and W' are given by
     H' = 1 + (H + 2 * pad - HH) / stride
     W' = 1 + (W + 2 * pad - WW) / stride
-  - cache: (x, w, b, conv_param)
-  """
-  out = None
-  pad = conv_param['pad']
-  stride = conv_param['stride']
+    - cache: (x, w, b, conv_param)
+    """
+    out = None
+    pad = conv_param['pad']
+    stride = conv_param['stride']
 
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement the forward pass of a convolutional neural network.
-  #   Store the output as 'out'.
-  #   Hint: to pad the array, you can use the function np.pad.
-  # ================================================================ #
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement the forward pass of a convolutional neural network.
+    #   Store the output as 'out'.
+    #   Hint: to pad the array, you can use the function np.pad.
+    # ================================================================ #
 
+    #get shapes
+    N, C, H, W = x.shape
+    F, _, hh, ww = w.shape
 
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
+    H_ = int(1 + (H + 2 * pad - hh) / stride)
+    W_ = int(1 + (W + 2 * pad - ww) / stride)
     
-  cache = (x, w, b, conv_param)
-  return out, cache
+    #sanity check
+    #print(f"N: {N}, F: {F}\nH: {H}, W: {W}\nH_: {H_}, W_: {W_}\nhh: {hh}, ww: {ww}")
+    
+    #pad the array
+    if pad != None:
+        x = np.pad(x, ((0,0), (0,0), (pad,pad), (pad,pad)), 'constant', constant_values=0)
+    
+    #convolve
+    out = np.zeros(shape=(N, F, H_, W_))
+
+    for n in range(N):
+        for f in range(F): 
+            for i in range(H_):
+                for j in range(W_):
+                    #print(f"n = {n}, f = {f}, i = {i}, j = {j}")
+                    sub = x[n, :, i*stride:(i*stride+hh), j*stride:(j*stride+ww)]             
+                    intermed = np.sum(sub * w[f])+ b[f] #hadamard product, summed, added to biases of each class
+                    out[n, f, i, j] = intermed
+            
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
+
+    cache = (x, w, b, conv_param)
+    return out, cache
 
 
 def conv_backward_naive(dout, cache):
-  """
-  A naive implementation of the backward pass for a convolutional layer.
+    """
+    A naive implementation of the backward pass for a convolutional layer.
 
-  Inputs:
-  - dout: Upstream derivatives.
-  - cache: A tuple of (x, w, b, conv_param) as in conv_forward_naive
+    Inputs:
+    - dout: Upstream derivatives.
+    - cache: A tuple of (x, w, b, conv_param) as in conv_forward_naive
 
-  Returns a tuple of:
-  - dx: Gradient with respect to x
-  - dw: Gradient with respect to w
-  - db: Gradient with respect to b
-  """
-  dx, dw, db = None, None, None
+    Returns a tuple of:
+    - dx: Gradient with respect to x
+    - dw: Gradient with respect to w
+    - db: Gradient with respect to b
+    """
+    dx, dw, db = None, None, None
 
-  N, F, out_height, out_width = dout.shape
-  x, w, b, conv_param = cache
-  
-  stride, pad = [conv_param['stride'], conv_param['pad']]
-  xpad = np.pad(x, ((0,0), (0,0), (pad,pad), (pad,pad)), mode='constant')
-  num_filts, _, f_height, f_width = w.shape
+    N, F, out_height, out_width = dout.shape
+    x, w, b, conv_param = cache
 
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement the backward pass of a convolutional neural network.
-  #   Calculate the gradients: dx, dw, and db.
-  # ================================================================ #
+    stride, pad = [conv_param['stride'], conv_param['pad']]
+    xpad = np.pad(x, ((0,0), (0,0), (pad,pad), (pad,pad)), mode='constant')
+    num_filts, _, f_height, f_width = w.shape
 
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement the backward pass of a convolutional neural network.
+    #   Calculate the gradients: dx, dw, and db.
+    # ================================================================ #
+    dw = np.zeros_like(w)
+    dx = np.zeros_like(x)
+    db = np.zeros_like(b)
+    
+    #print(f"shape of dout: {dout.shape}")
+    #print(f"shape of dx: {dx.shape}")
+    #print(f"shape of dw: {dw.shape}")
+    #print(f"shape of db: {db.shape}")
+    
+    for n in range(N):
+        for f in range(F):
+            for i in range(out_height):
+                for j in range(out_width):
+                    #print(f"n = {n}, f = {f}, i = {i}, j = {j}")
+                    dx[n, :, i*stride:(i*stride+f_height), j*stride:(j*stride+f_width)] += w[f]*dout[n,f,i,j]
+                    dw[f] += x[n, :, i*stride:(i*stride+f_height), j*stride:(j*stride+f_width)]*dout[n,f,i,j]
+    db = np.sum(dout, axis=(0,2,3))
+    dx = dx[:, :, pad:-pad, pad:-pad]
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
 
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
-
-  return dx, dw, db
+    return dx, dw, db
 
 
 def max_pool_forward_naive(x, pool_param):
-  """
-  A naive implementation of the forward pass for a max pooling layer.
+    """
+    A naive implementation of the forward pass for a max pooling layer.
 
-  Inputs:
-  - x: Input data, of shape (N, C, H, W)
-  - pool_param: dictionary with the following keys:
+    Inputs:
+    - x: Input data, of shape (N, C, H, W)
+    - pool_param: dictionary with the following keys:
     - 'pool_height': The height of each pooling region
     - 'pool_width': The width of each pooling region
     - 'stride': The distance between adjacent pooling regions
 
-  Returns a tuple of:
-  - out: Output data
-  - cache: (x, pool_param)
-  """
-  out = None
-  
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement the max pooling forward pass.
-  # ================================================================ #
+    Returns a tuple of:
+    - out: Output data
+    - cache: (x, pool_param)
+    """
+    out = None
 
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement the max pooling forward pass.
+    # ================================================================ #
+    pH = pool_param['pool_height']
+    pW = pool_param['pool_width']
+    s = pool_param['stride']
+    
+    N,C,H,W = x.shape
 
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ # 
-  cache = (x, pool_param)
-  return out, cache
+    H_ = int((H - pH) / s + 1)
+    W_ = int((W - pW) / s + 1)
+    
+    #sanity check
+    #print(f"N: {C}, F: {C}\nH: {H}, W: {W}\nH_: {H_}, W_: {W_}\npH: {pH}, pW: {pW}\nstride: {s}")
+    
+    #initialize out
+    out = np.zeros( (N,C,H_,W_) )
+    
+    for i in range(H_):
+        for j in range(W_):
+            sub = x[:, :, i*s:(i*s+pH), j*s:(j*s+pW)]
+            #print(f"shape of sub: {sub.shape}")
+            out[:, :, i, j] = (sub).max(axis=(2,3))
+    
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ # 
+    cache = (x, pool_param)
+    return out, cache
 
 def max_pool_backward_naive(dout, cache):
-  """
-  A naive implementation of the backward pass for a max pooling layer.
+    """
+    A naive implementation of the backward pass for a max pooling layer.
 
-  Inputs:
-  - dout: Upstream derivatives
-  - cache: A tuple of (x, pool_param) as in the forward pass.
+    Inputs:
+    - dout: Upstream derivatives
+    - cache: A tuple of (x, pool_param) as in the forward pass.
 
-  Returns:
-  - dx: Gradient with respect to x
-  """
-  dx = None
-  x, pool_param = cache
-  pool_height, pool_width, stride = pool_param['pool_height'], pool_param['pool_width'], pool_param['stride']
+    Returns:
+    - dx: Gradient with respect to x
+    """
+    dx = None
+    x, pool_param = cache
+    pool_height, pool_width, stride = pool_param['pool_height'], pool_param['pool_width'], pool_param['stride']
 
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement the max pooling backward pass.
-  # ================================================================ #
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement the max pooling backward pass.
+    # ================================================================ #
+    pH = pool_param['pool_height']
+    pW = pool_param['pool_width']
+    s = pool_param['stride']
+    
+    N,C,H,W = x.shape
 
+    H_ = int((H - pH) / s + 1)
+    W_ = int((W - pW) / s + 1)
+    
+    dx=np.zeros(x.shape)
 
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ # 
+    for n in range(N):
+        for c in range(C):
+            for i in range(H_):
+                for j in range(W_):
+                    sub = x[n,c,i*s:i*s+pH,j*s:j*s+pW]
+                    max_ind1, max_ind2 = np.unravel_index(np.argmax(sub), (pH, pW))
+                    dmax = np.zeros((pH,pW))
+                    dmax[max_ind1, max_ind2] = dout[n,c,i,j]
+                    #print(f"dmax:\n{dmax}")
+                    dx[n,c,i*s:i*s+pH,j*s:j*s+pW] = dmax
 
-  return dx
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ # 
+
+    return dx
 
 def spatial_batchnorm_forward(x, gamma, beta, bn_param):
   """
--- hw5/nndl/optim.py
+++ hw5/nndl/optim.py
@@ -41,158 +41,190 @@ setting next_w equal to w.
 
 
 def sgd(w, dw, config=None):
-  """
-  Performs vanilla stochastic gradient descent.
+    """
+    Performs vanilla stochastic gradient descent.
 
-  config format:
-  - learning_rate: Scalar learning rate.
-  """
-  if config is None: config = {}
-  config.setdefault('learning_rate', 1e-2)
+    config format:
+    - learning_rate: Scalar learning rate.
+    """
+    if config is None: config = {}
+    config.setdefault('learning_rate', 1e-2)
 
-  w -= config['learning_rate'] * dw
-  return w, config
+    w -= config['learning_rate'] * dw
+    return w, config
 
 
 def sgd_momentum(w, dw, config=None):
-  """
-  Performs stochastic gradient descent with momentum.
+    """
+    Performs stochastic gradient descent with momentum.
 
-  config format:
-  - learning_rate: Scalar learning rate.
-  - momentum: Scalar between 0 and 1 giving the momentum value.
-    Setting momentum = 0 reduces to sgd.
-  - velocity: A numpy array of the same shape as w and dw used to store a moving
-    average of the gradients.
-  """
-  if config is None: config = {}
-  config.setdefault('learning_rate', 1e-2)
-  config.setdefault('momentum', 0.9) # set momentum to 0.9 if it wasn't there
-  v = config.get('velocity', np.zeros_like(w))	 # gets velocity, else sets it to zero.
- 
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement the momentum update formula.  Return the updated weights
-  #   as next_w, and the updated velocity as v.
-  # ================================================================ #
-
-
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
+    config format:
+    - learning_rate: Scalar learning rate.
+    - momentum: Scalar between 0 and 1 giving the momentum value.
+      Setting momentum = 0 reduces to sgd.
+    - velocity: A numpy array of the same shape as w and dw used to store a moving
+      average of the gradients.
+    """
+    if config is None: config = {}
+    config.setdefault('learning_rate', 1e-2)
+    config.setdefault('momentum', 0.9) # set momentum to 0.9 if it wasn't there
+    v = config.get('velocity', np.zeros_like(w))   # gets velocity, else sets it to zero.
     
-  config['velocity'] = v
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement the momentum update formula.  Return the updated weights
+    #   as next_w, and the updated velocity as v.
+    # ================================================================ #
+    v = config['momentum'] * v - config['learning_rate']*dw
+    next_w = w + v
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
+    
+    config['velocity'] = v
 
-  return next_w, config
+    return next_w, config
 
 def sgd_nesterov_momentum(w, dw, config=None):
-  """
-  Performs stochastic gradient descent with Nesterov momentum.
+    """
+    Performs stochastic gradient descent with Nesterov momentum.
 
-  config format:
-  - learning_rate: Scalar learning rate.
-  - momentum: Scalar between 0 and 1 giving the momentum value.
-    Setting momentum = 0 reduces to sgd.
-  - velocity: A numpy array of the same shape as w and dw used to store a moving
-    average of the gradients.
-  """
-  if config is None: config = {}
-  config.setdefault('learning_rate', 1e-2)
-  config.setdefault('momentum', 0.9) # set momentum to 0.9 if it wasn't there
-  v = config.get('velocity', np.zeros_like(w))	 # gets velocity, else sets it to zero.
- 
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement the momentum update formula.  Return the updated weights
-  #   as next_w, and the updated velocity as v.
-  # ================================================================ #
-  
-
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
+    config format:
+    - learning_rate: Scalar learning rate.
+    - momentum: Scalar between 0 and 1 giving the momentum value.
+      Setting momentum = 0 reduces to sgd.
+    - velocity: A numpy array of the same shape as w and dw used to store a moving
+      average of the gradients.
+    """
+    if config is None: config = {}
+    config.setdefault('learning_rate', 1e-2)
+    config.setdefault('momentum', 0.9) # set momentum to 0.9 if it wasn't there
+    v = config.get('velocity', np.zeros_like(w))   # gets velocity, else sets it to zero.
     
-  config['velocity'] = v
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement the momentum update formula.  Return the updated weights
+    #   as next_w, and the updated velocity as v.
+    # ================================================================ #
+    
+    v_new = config['momentum'] *v - config['learning_rate']*dw
+    next_w = w + v_new + config['momentum']*(v_new - v)
+    
+    v = v_new
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
+    
+    config['velocity'] = v
 
-  return next_w, config
+    return next_w, config
 
 def rmsprop(w, dw, config=None):
-  """
-  Uses the RMSProp update rule, which uses a moving average of squared gradient
-  values to set adaptive per-parameter learning rates.
+    """
+    Uses the RMSProp update rule, which uses a moving average of squared gradient
+    values to set adaptive per-parameter learning rates.
 
-  config format:
-  - learning_rate: Scalar learning rate.
-  - decay_rate: Scalar between 0 and 1 giving the decay rate for the squared
-    gradient cache.
-  - epsilon: Small scalar used for smoothing to avoid dividing by zero.
-  - beta: Moving average of second moments of gradients.
-  """
-  if config is None: config = {}
-  config.setdefault('learning_rate', 1e-2)
-  config.setdefault('decay_rate', 0.99)
-  config.setdefault('epsilon', 1e-8)
-  config.setdefault('a', np.zeros_like(w))
+    config format:
+    - learning_rate: Scalar learning rate.
+    - decay_rate: Scalar between 0 and 1 giving the decay rate for the squared
+      gradient cache.
+    - epsilon: Small scalar used for smoothing to avoid dividing by zero.
+    - beta: Moving average of second moments of gradients.
+    """
+    if config is None: config = {}
+    config.setdefault('learning_rate', 1e-2)
+    config.setdefault('decay_rate', 0.99)
+    config.setdefault('epsilon', 1e-8)
+    config.setdefault('a', np.zeros_like(w))
 
-  next_w = None
+    next_w = None
 
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement RMSProp.  Store the next value of w as next_w.  You need
-  #   to also store in config['a'] the moving average of the second
-  #   moment gradients, so they can be used for future gradients. Concretely,
-  #   config['a'] corresponds to "a" in the lecture notes.
-  # ================================================================ #
-  
-
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement RMSProp.  Store the next value of w as next_w.  You need
+    #   to also store in config['a'] the moving average of the second
+    #   moment gradients, so they can be used for future gradients. Concretely,
+    #   config['a'] corresponds to "a" in the lecture notes.
+    # ================================================================ #
+    decay = config['decay_rate']
+    a = decay*config['a'] + (1-decay)*np.multiply(dw,dw)
+    lr = config['learning_rate']/(np.sqrt(a) + config['epsilon'])
+    next_w = w - np.multiply(lr, dw)
+    config['a'] = a
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
     
-  return next_w, config
+    return next_w, config
 
 
 def adam(w, dw, config=None):
-  """
-  Uses the Adam update rule, which incorporates moving averages of both the
-  gradient and its square and a bias correction term.
+    """
+    Uses the Adam update rule, which incorporates moving averages of both the
+    gradient and its square and a bias correction term.
 
-  config format:
-  - learning_rate: Scalar learning rate.
-  - beta1: Decay rate for moving average of first moment of gradient.
-  - beta2: Decay rate for moving average of second moment of gradient.
-  - epsilon: Small scalar used for smoothing to avoid dividing by zero.
-  - m: Moving average of gradient.
-  - v: Moving average of squared gradient.
-  - t: Iteration number.
-  """
-  if config is None: config = {}
-  config.setdefault('learning_rate', 1e-3)
-  config.setdefault('beta1', 0.9)
-  config.setdefault('beta2', 0.999)
-  config.setdefault('epsilon', 1e-8)
-  config.setdefault('v', np.zeros_like(w))
-  config.setdefault('a', np.zeros_like(w))
-  config.setdefault('t', 0)
-  
-  next_w = None
-
-  # ================================================================ #
-  # YOUR CODE HERE:
-  #   Implement Adam.  Store the next value of w as next_w.  You need
-  #   to also store in config['a'] the moving average of the second
-  #   moment gradients, and in config['v'] the moving average of the
-  #   first moments.  Finally, store in config['t'] the increasing time.
-  # ================================================================ #
-
-
-  # ================================================================ #
-  # END YOUR CODE HERE
-  # ================================================================ #
+    config format:
+    - learning_rate: Scalar learning rate.
+    - beta1: Decay rate for moving average of first moment of gradient.
+    - beta2: Decay rate for moving average of second moment of gradient.
+    - epsilon: Small scalar used for smoothing to avoid dividing by zero.
+    - m: Moving average of gradient.
+    - v: Moving average of squared gradient.
+    - t: Iteration number.
+    """
+    if config is None: config = {}
+    config.setdefault('learning_rate', 1e-3)
+    config.setdefault('beta1', 0.9)
+    config.setdefault('beta2', 0.999)
+    config.setdefault('epsilon', 1e-8)
+    config.setdefault('v', np.zeros_like(w))
+    config.setdefault('a', np.zeros_like(w))
+    config.setdefault('t', 0)
     
-  return next_w, config
+    next_w = None
 
-  
-  
-  
+    # ================================================================ #
+    # YOUR CODE HERE:
+    #   Implement Adam.  Store the next value of w as next_w.  You need
+    #   to also store in config['a'] the moving average of the second
+    #   moment gradients, and in config['v'] the moving average of the
+    #   first moments.  Finally, store in config['t'] the increasing time.
+    # ================================================================ #
+    
+    #get constants
+    beta1 = config['beta1']
+    beta2 = config['beta2']
+    eps = config['epsilon']
+    v = config['v']
+    a = config['a']
+    lr = config['learning_rate']
+    it = config['t']
+    
+    #update iteration
+    it +=1   
+    config['t'] =it
+    
+    #momentum-like update
+    v = beta1*v + (1-beta1)*dw
+    config['v']=v
 
+    #gradient normalization
+    a = beta2*a + (1-beta2)*np.multiply(dw,dw)
+    config['a']=a
+    
+    #bias correction
+    v = 1/(1-beta1**it)*v
+    a = 1/(1-beta2**it)*a
+    
+    #new learning rate
+    lr = lr/(np.sqrt(a) + eps)
+    
+    #gradient step
+    next_w = w - np.multiply(lr,v)
+    
+    
+    # ================================================================ #
+    # END YOUR CODE HERE
+    # ================================================================ #
+    
+    return next_w, config
